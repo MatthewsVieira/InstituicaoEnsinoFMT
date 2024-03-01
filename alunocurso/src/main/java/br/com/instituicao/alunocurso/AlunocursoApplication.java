7968849314012eff763857d4a8a9bef4c5e8529a
Matheus <112503237+MatthewsVieira@users.noreply.github.com>
2024-03-01 11:05:32 -0300

    Add: Spring

---
+++ alunocurso/src/main/java/br/com/instituicao/alunocurso/AlunocursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.instituicao.alunocurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunocursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunocursoApplication.class, args);
+	}
+
+}
