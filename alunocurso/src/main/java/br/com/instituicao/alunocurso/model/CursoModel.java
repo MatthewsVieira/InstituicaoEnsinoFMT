b5b592a7615ccd19c688d84dc071280853bf626b
Matheus <112503237+MatthewsVieira@users.noreply.github.com>
2024-03-03 11:07:39 -0300

    Cria Classe Curso

--- alunocurso/src/main/java/br/com/instituicao/alunocurso/model/CursoModel.java
+++ alunocurso/src/main/java/br/com/instituicao/alunocurso/model/CursoModel.java
@@ -4,19 +4,18 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
 
-import java.time.LocalDate;
-
 @Data
-public class AlunoModel {
+public class CursoModel {
 
     private static Integer proximoId = 1;
 
     @Setter(AccessLevel.NONE) private Integer id;
     private String nome;
-    private LocalDate dataNascimento;
+    private String descricao;
+    private Integer cargaHoraria;
 
     private Integer getProximoId() {
         return proximoId++;
     }
-
+    
 }
